d7a2b8c4b60622dd053223f46043cfd67b0d5e06
Tahmid Hassan <tahmid.hassan2002@gmail.com>
2023-05-22 20:21:33 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -93,10 +93,10 @@ async def start(_, message):
     elif config_dict['DM_MODE']:
         start_string = 'Bot Started.\n' \
                        'Now I can send your stuff here.\n' \
-                       'Use me here: @Z_Mirror'
+                       'Bot of: @MHDZubalR'
     else:
         start_string = 'Sorry, you cant use me here!\n' \
-                       'Join @Z_Mirror to use me.\n' \
+                       'Contact @MHDZubalR to use me.\n' \
                        'Thank You'
     await sendMessage(message, start_string)
 
